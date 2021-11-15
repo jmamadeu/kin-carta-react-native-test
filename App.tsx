01dcd3d0d838531f79fdbbfe5b607f04b7ba4264
jmamadeu <jmamadeu2000@gmail.com>
2021-11-15 04:14:56 +0100

    fix: react v17 support

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
+      <Text>Hello world with styles ss</Text>
       <StatusBar style="auto" />
     </View>
   );
