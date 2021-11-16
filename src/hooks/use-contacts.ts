19ed2c279cba583142db80634cc3fbc4a333764e
jmamadeu <jmamadeu2000@gmail.com>
2021-11-16 23:41:25 +0100

    chore: refactor parese contacts funciton

--- src/hooks/use-contacts.ts
+++ src/hooks/use-contacts.ts
@@ -2,22 +2,12 @@ import { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
 import { ContactProperties, ContactSectionedByFavoriteProperties } from '../models/contact';
 import { api } from '../services/api';
+import { parseContactsByFavorite } from '../utils/contacts';
 
 const fetchAllContacts = async (): Promise<ContactSectionedByFavoriteProperties[]> => {
   const { data: contacts } = await api.get<ContactProperties[]>('/contacts.json');
 
-  const parsedContacts: ContactSectionedByFavoriteProperties[] = [
-    {
-      title: 'Favorites Contacts',
-      isFavorite: true,
-      data: contacts.filter((contact) => contact.isFavorite)
-    },
-    {
-      title: 'Others Contacts',
-      isFavorite: false,
-      data: contacts.filter((contact) => !contact.isFavorite)
-    }
-  ];
+  const parsedContacts = parseContactsByFavorite(contacts);
 
   return parsedContacts;
 };
--- src/utils/contacts.ts
+++ src/utils/contacts.ts
@@ -1,4 +1,5 @@
-import { ContactProperties } from '../models/contact';
+import { ContactProperties, ContactSectionedByFavoriteProperties } from '../models/contact';
+import { queryClient } from '../services/api';
 
 export function sortContactsByName(section: ContactProperties[]): ContactProperties[] {
   const sectionSorted = section
@@ -7,4 +8,29 @@ export function sortContactsByName(section: ContactProperties[]): ContactPropert
     ?.map((cont) => section?.find((d) => d.name === cont) || ({} as ContactProperties));
 
   return sectionSorted || [];
+}
+
+export function getContactsParsedByFavorite() {
+  const contacts =
+    queryClient.getQueryData<ContactSectionedByFavoriteProperties[]>('contacts') || [];
+  const contactsParsed = [...(contacts?.[0].data || []), ...contacts[1].data];
+
+  return contactsParsed;
+}
+
+export function parseContactsByFavorite(contacts: ContactProperties[]) {
+  const parsedContacts: ContactSectionedByFavoriteProperties[] = [
+    {
+      title: 'Favorites Contacts',
+      isFavorite: true,
+      data: contacts.filter((contact) => contact.isFavorite)
+    },
+    {
+      title: 'Others Contacts',
+      isFavorite: false,
+      data: contacts.filter((contact) => !contact.isFavorite)
+    }
+  ];
+
+  return parsedContacts;
 }
